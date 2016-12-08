c1fc624b359bec62ec1b3c85ab68fc6853d3a34b
Ernesto Celis <developer@celisdelafuente.net>
2016-12-07 23:52:07 -0600

    initial commit

---
+++ spec/language-avia-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageAviaView from '../lib/language-avia-view';
+
+describe('LanguageAviaView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
